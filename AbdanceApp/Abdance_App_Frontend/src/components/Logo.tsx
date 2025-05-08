eb5729f1546ea1e923c1dac3e61e01a194457b79
KarmePz <baezjulicarme16@gmail.com>
2025-05-08 19:20:01 -0300

    se realizo pantalla index

---
+++ AbdanceApp/Abdance_App_Frontend/src/components/Logo.tsx
@@ -0,0 +1,16 @@
+
+
+function Logo() {
+    
+    return (
+        <div className="fixed top-20 left-0 right-[10%] flex justify-center z-50 ">
+
+            <img src="/Logo.png" className="relative -top-10 mx-auto" alt="Logo">
+            </img>
+        
+        </div>
+        
+    )
+}
+
+export default Logo
