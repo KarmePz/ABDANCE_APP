8390b2ba9a86ed116dc25bd5252d274c092f98ae
KarmePz <baezjulicarme16@gmail.com>
2025-05-08 19:58:00 -0300

    add ico y title

--- AbdanceApp/Abdance_App_Frontend/src/App.tsx
+++ AbdanceApp/Abdance_App_Frontend/src/App.tsx
@@ -1,10 +1,13 @@
 
 import './App.css'
 import {Background, Logo} from './components'
+import { useDocumentTitle } from './hooks/useDocumentTitle';
+import { useFavicon } from './hooks/useFavicon';
 
 function App() {
 
-
+  useDocumentTitle("Academia ABDANCE");
+  useFavicon("/dance.ico")
   return (
     <>
       <Background />
