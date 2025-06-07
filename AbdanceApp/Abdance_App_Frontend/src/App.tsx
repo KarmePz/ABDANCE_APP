f849ca7be57e225a69aa6cf5c04409cf654f64eb
Agustin Schipani <agus-calderon-schip03@outlook.com>
2025-06-07 02:02:35 -0300

    Tabla de Cuotas y Pago Manual
    Tabla de cuotas y Pago manual hecho, (no tienen las URL de la API, porque en el Main aun no estan esas funciones).
    Tambien elimino firebase-config.ts

--- AbdanceApp/Abdance_App_Frontend/src/App.tsx
+++ AbdanceApp/Abdance_App_Frontend/src/App.tsx
@@ -13,15 +13,15 @@ function App() {
   
   return (
     <>
-        <PagosTest></PagosTest>
-        {/* <Background />
+        {/* <PagosTest></PagosTest> */}
+        <Background />
         <Logo /> 
         <div className="min-h-screen flex flex-col items-center justify-center px-4">
           <LoginForm ></LoginForm>
 
 
           <h3 className="mt-10 text-center text-white"> ¿Te interesa saber sobre nuestros eventos? <Link to="/eventos"  target='blank'>¡Hace click Aqui!</Link></h3>
-        </div> */}
+        </div>
     </>
   )
 }
--- AbdanceApp/Abdance_App_Frontend/src/AppRouter.tsx
+++ AbdanceApp/Abdance_App_Frontend/src/AppRouter.tsx
@@ -3,6 +3,7 @@ import {  Navigate, Route, Routes } from "react-router-dom";
 import App from "./App";
 import { PrivateGuard } from "./guard/PrivateGuard";
 import {DisciplinaTable, UserTable} from './components'
+import { CuotaTable } from "./components/CuotaTable";
 import Dashboard from "./pages/Dashboard";
 
 
@@ -42,7 +43,7 @@ export const AppRouter = ({children}:Props) =>{
                             <Route path="/dashboard/asistencias/alumno_dni" element={<Dashboard/>} />
                             
 
-                            <Route path="/dashboard/cuotas" element={<h1>Cuotas</h1>} />
+                            <Route path="/dashboard/cuotas" element={<CuotaTable />} />
                             <Route path="/dashboard/cuotas/alumno_dni" element={<Dashboard/>} />
                             <Route path="/dashboard/cuotas/alumno_dni/id_cuota" element={<Dashboard/>} />
 
