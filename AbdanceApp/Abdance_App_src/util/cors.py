d5d97ee6c415bda5ace7178eeaf40770cfb0b152
Agustin Schipani <agus-calderon-schip03@outlook.com>
2025-06-21 22:19:57 -0300

    Update cors.py
    ..

--- AbdanceApp/Abdance_App_src/util/cors.py
+++ AbdanceApp/Abdance_App_src/util/cors.py
@@ -2,13 +2,7 @@ from flask import jsonify, make_response
 
 
 def apply_cors(response_tuple):
-    # headers = {
-    #     'Access-Control-Allow-Origin': 'http://localhost:5173',  # o '*' para todos
-    #     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    #     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    # }
     data, status = response_tuple
     response = make_response(jsonify(data), status)
     response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
     return response
-    # return (response_body, status, headers)
